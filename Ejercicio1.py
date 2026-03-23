caa52665a7346f52247277034916fc49a126f4c6
jessshacks <jesssh8glg@gmail.com>
2026-03-23 12:54:06 -0600

    condicionales.py
    Clase:programacion

---
+++ Ejercicio1.py
@@ -0,0 +1,42 @@
+#1
+altura=1.77
+nombre="Jesus"
+
+if nombre=="Jesus" and altura==1.77:
+    print("El nombre es Jesus")
+    print("La altura es 1.77")
+#2
+ño = "no"
+ti = "si"
+
+if ño == "no":
+        print("ño es no")
+        print("TI es si")
+#3
+Perro="canino"
+carro="vehículo"
+
+if Perro == "canino":
+    print("Perro es canino")
+    print("Carro es vehículo")
+#4
+manzana="pera"
+mandarina="naranja"
+
+if mandarina == "naranja":
+    print("manzana es pera")
+    print("mandarina es naranja")
+#5
+compu="pc"
+play="consola"
+
+if play=="consola":
+    print("compu es pc")
+    print("play es consola")
+#6
+morir="descanso eterno"
+dormir="descansar"
+
+if dormir=="descansar":
+    print("morir es descanso eterno")
+    print("dormir es descansar")
